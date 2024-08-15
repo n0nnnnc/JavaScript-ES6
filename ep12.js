c5657b034ad9596fd250036dcbc3143e657d1d21
n0nnnnc <napatchirawichada@gmail.com>
2024-08-15 21:55:38 +0700

    add ep14.js and format ep12.js

--- ep12.js
+++ ep12.js
@@ -14,6 +14,5 @@ const data = [10, 20, 30, 40, 50];
 
 for (const element of data) {
     if(element >= 30) break;
-    console.log(`สมาชิก Array Data = ${element}`);
-    
-}
+    console.log(`สมาชิก Array Data = ${element}`);   
+};
